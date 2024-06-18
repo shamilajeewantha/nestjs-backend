deb30b89cfc5c1fcf26d353780604d1744ed2e2e
Jeewantha H.M.S <119792484+shamilajeewantha@users.noreply.github.com>
2024-06-18 18:01:09 +0530

    insert dummy

--- src/auth/customer/customer.entity.ts
+++ src/auth/customer/customer.entity.ts
@@ -24,3 +24,21 @@ export class Customer {
   @OneToMany(() => Order, (order) => order.customer)
   orders: Order[];
 }
+
+
+/* 
+
+INSERT INTO customer (first_name, last_name, email, "password", phone_number)
+VALUES
+('Michael', 'Smith', 'msmith@email.com', '5', '1234567890'),
+('Jennifer', 'Johnson', 'jjohnson@email.com', '5', '9876543210'),
+('Christopher', 'Williams', 'cwilliams@email.com', '5', '5551234567'),
+('Jessica', 'Jones', 'jjones@email.com', '5', '1112223333'),
+('Matthew', 'Brown', 'mbrown@email.com', '5', '9998887777'),
+('Amanda', 'Davis', 'adavis@email.com', '5', '4445556666'),
+('Sarah', 'Martinez', 'smartinez@email.com', '5', '3334445555'),
+('Daniel', 'Miller', 'dmiller@email.com', '5', '2223334444'),
+('Elizabeth', 'Wilson', 'ewilson@email.com', '5', '6667778888'),
+('James', 'Moore', 'jmoore@email.com', '5', '7778889999');
+
+ */
--- src/auth/shop/shop.entity.ts
+++ src/auth/shop/shop.entity.ts
@@ -25,3 +25,21 @@ export class Shop {
   @OneToMany(() => Order, (order) => order.shop)
   orders: Order[];
 }
+
+
+
+/* 
+
+INSERT INTO shop (shop_name, address, phone, email, "password") VALUES
+('Gucci', 'Via de Tornabuoni, 73R, 50123 Firenze FI, Italy', '055759221', 'G', '1'),
+('Prada', 'Galleria Vittorio Emanuele II, 20121 Milano MI, Italy', '026572333', 'P', '1'),
+('Louis Vuitton', '101 Avenue des Champs-Élysées, 75008 Paris, France', '0140531600', 'L', '1'),
+('Burberry', '121 Regent St, London W1B 4TB, United Kingdom', '02078068000', 'B', '1'),
+('Hermès', '24 Rue du Faubourg Saint-Honoré, 75008 Paris, France', '0140174700', 'H', '1'),
+('Dior', '30 Avenue Montaigne, 75008 Paris, France', '0144075250', 'D', '1'),
+('Chanel', '31 Rue Cambon, 75001 Paris, France', '0144746800', 'C', '1'),
+('Ralph Lauren', '867 Madison Ave, New York, NY 10021, United States', '0212356898', 'R', '1'),
+('Versace', 'Via Gesù, 12, 20121 Milano MI, Italy', '0286798311', 'V', '1'),
+('Fendi', 'Via Borgognona, 40, 00187 Roma RM, Italy', '066992566', 'F', '1');
+
+ */
